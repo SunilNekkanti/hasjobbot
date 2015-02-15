f36a2975c8cddf122085615f098fcc6461cd1c8c
Jaysinh Shukla <jaysinhp@gmail.com>
2015-02-15 20:00:25 +0530

    [ADD] creating proper destribution for the package using setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="Hasjob BOT",
+    version="0.0.2",
+    author="Jaysinh Shukla",
+    author_email="jaysinhp@gmail.com",
+    description="Allows to automate the job application process at hasjob.co",
+    keywords="hasjob bot hasjobbot",
+    packages=["hasjobbot"],
+    long_description="I am yet to provide long description",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Framework :: mechanize",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Logging",
+    ],
+)
